2edd29e27010fbd3f67e1aa5d607d921a1078a4d
Thiago <thiago@lepidus.com.br>
2024-09-30 22:21:26 -0400

    Creates issue model class
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#892
    
    Signed-off-by: Thiago <thiago@lepidus.com.br>

---
+++ src/GraphQL/Model/Issue.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ThothClient\GraphQL\Model;
+
+class Issue extends AbstractModel
+{
+    public function getIssueId(): string
+    {
+        return $this->getData('issueId');
+    }
+
+    public function setIssueId(string $issueId): void
+    {
+        $this->setData('issueId', $issueId);
+    }
+
+    public function getWorkId(): string
+    {
+        return $this->getData('workId');
+    }
+
+    public function setWorkId(string $workId): void
+    {
+        $this->setData('workId', $workId);
+    }
+
+    public function getSeriesId(): string
+    {
+        return $this->getData('seriesId');
+    }
+
+    public function setSeriesId(string $seriesId): void
+    {
+        $this->setData('seriesId', $seriesId);
+    }
+
+    public function getIssueOrdinal(): int
+    {
+        return $this->getData('issueOrdinal');
+    }
+
+    public function setIssueOrdinal(int $issueOrdinal): void
+    {
+        $this->setData('issueOrdinal', $issueOrdinal);
+    }
+}
---
+++ tests/GraphQL/Model/IssueTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ThothClient\Tests\GraphQL\Model;
+
+use PHPUnit\Framework\TestCase;
+use ThothClient\GraphQL\Model\Issue;
+
+class IssueTest extends TestCase
+{
+    public function testGettersAndSetters(): void
+    {
+        $issueId = '58d488a8-24c8-4efd-870b-0d62fc78a197';
+        $workId = 'd6dba809-eb49-4211-a9d3-05b3c2acfecf';
+        $seriesId = 'f09f7a9e-147b-444b-af1f-480dbd8a181e';
+        $issueOrdinal = 1;
+
+        $issue = new Issue();
+        $issue->setIssueId($issueId);
+        $issue->setWorkId($workId);
+        $issue->setSeriesId($seriesId);
+        $issue->setIssueOrdinal($issueOrdinal);
+
+        $this->assertSame($issueId, $issue->getIssueId());
+        $this->assertSame($workId, $issue->getWorkId());
+        $this->assertSame($seriesId, $issue->getSeriesId());
+        $this->assertSame($issueOrdinal, $issue->getIssueOrdinal());
+    }
+}
