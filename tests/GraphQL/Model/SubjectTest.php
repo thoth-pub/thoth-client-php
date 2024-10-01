69e9effc819a358d7618f460f0582cb8e1035c81
Thiago <thiago@lepidus.com.br>
2024-10-01 16:59:55 -0400

    Creates subject model class
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#892
    
    Signed-off-by: Thiago <thiago@lepidus.com.br>

---
+++ tests/GraphQL/Model/SubjectTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ThothClient\Tests\GraphQL\Model;
+
+use PHPUnit\Framework\TestCase;
+use ThothClient\GraphQL\Model\Subject;
+
+final class SubjectTest extends TestCase
+{
+    public function testGettersAndSetters(): void
+    {
+        $subjectId = '9ffc03b9-d6e2-43e9-a9bb-d89b78e29d3b';
+        $workId = '40ee65cd-b1ab-47ee-bc32-efca41747f57';
+        $subjectType = Subject::SUBJECT_TYPE_BIC;
+        $subjectCode = 'Foo';
+        $subjectOrdinal = 1;
+
+        $subject = new Subject();
+        $subject->setSubjectId($subjectId);
+        $subject->setWorkId($workId);
+        $subject->setSubjectType($subjectType);
+        $subject->setSubjectCode($subjectCode);
+        $subject->setSubjectOrdinal($subjectOrdinal);
+
+        $this->assertSame($subjectId, $subject->getSubjectId());
+        $this->assertSame($workId, $subject->getWorkId());
+        $this->assertSame($subjectType, $subject->getSubjectType());
+        $this->assertSame($subjectCode, $subject->getSubjectCode());
+        $this->assertSame($subjectOrdinal, $subject->getSubjectOrdinal());
+    }
+}
