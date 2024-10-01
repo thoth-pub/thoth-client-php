4187a9f4cc6756dc3dd9ffeda8122cccfb00986b
Thiago <thiago@lepidus.com.br>
2024-10-01 15:20:47 -0400

    Creates publisher model class
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#892
    
    Signed-off-by: Thiago <thiago@lepidus.com.br>

---
+++ src/GraphQL/Model/Publisher.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ThothClient\GraphQL\Model;
+
+class Publisher extends AbstractModel
+{
+    public function getPublisherId(): string
+    {
+        return $this->getData('publisherId');
+    }
+
+    public function setPublisherId(string $publisherId): void
+    {
+        $this->setData('publisherId', $publisherId);
+    }
+
+    public function getPublisherName(): string
+    {
+        return $this->getData('publisherName');
+    }
+
+    public function setPublisherName(string $publisherName): void
+    {
+        $this->setData('publisherName', $publisherName);
+    }
+
+    public function getPublisherShortName(): string
+    {
+        return $this->getData('publisherShortName');
+    }
+
+    public function setPublisherShortName(string $publisherShortName): void
+    {
+        $this->setData('publisherShortName', $publisherShortName);
+    }
+
+    public function getPublisherUrl(): string
+    {
+        return $this->getData('publisherUrl');
+    }
+
+    public function setPublisherUrl(string $publisherUrl): void
+    {
+        $this->setData('publisherUrl', $publisherUrl);
+    }
+}
