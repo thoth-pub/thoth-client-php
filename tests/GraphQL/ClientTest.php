0c8f5ec00f0a74a7c5e4f19ca4a1dcf06fa169c9
Thiago Brasil <thiago@lepidus.com.br>
2025-01-22 11:09:36 -0400

    Added method to run raw queries

--- tests/GraphQL/ClientTest.php
+++ tests/GraphQL/ClientTest.php
@@ -1819,4 +1819,46 @@ final class ClientTest extends TestCase
         $result = $this->client->deleteWorkRelation($workRelation);
         $this->assertSame('64019725-3e50-41de-a785-e4e5ddc4620a', $result);
     }
+
+    public function testRunRawQuery(): void
+    {
+        $this->mockHandler->append(new Response(200, [], json_encode([
+            'data' => [
+                'books' => [
+                    'fullTitle' => 'My book title',
+                    'doi' => 'https://doi.org/10.123435/12345678',
+                    'publications' => [],
+                    'contributions' => []
+                ]
+            ]
+        ])));
+
+        $query = <<<GQL
+        books(order: {field: PUBLICATION_DATE, direction: ASC}) {
+            fullTitle
+            doi
+            publications {
+                    publicationType
+                    isbn
+            }
+            contributions {
+                    contributionType
+                    fullName
+            }
+        }
+        GQL;
+
+        $args = [];
+
+        $result = $this->client->rawQuery($query, $args);
+
+        $this->assertSame([
+            'books' => [
+                'fullTitle' => 'My book title',
+                'doi' => 'https://doi.org/10.123435/12345678',
+                'publications' => [],
+                'contributions' => []
+            ]
+        ], $result);
+    }
 }
